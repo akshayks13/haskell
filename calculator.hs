5bb3b5ded07963ea282ce9cbf393dc31a51c1f31
Akshay KS <143313500+akshayks13@users.noreply.github.com>
2025-06-01 19:54:45 +0530

    Update calculator.hs

--- calculator.hs
+++ calculator.hs
@@ -1,20 +1,21 @@
 
--- calculate :: Int -> Int -> Char -> Int
--- calculate a b op
---     | op == '+' = a + b
---     | op == '-' = a - b
---     | op == '*' = a * b
---     | op == '/' = a `div` b  -- Integer division
---     | otherwise = error "Invalid operation"
+-- Using Guards
+calculate :: Int -> Int -> Char -> Int
+calculate a b op
+    | op == '+' = a + b
+    | op == '-' = a - b
+    | op == '*' = a * b
+    | op == '/' = a `div` b  -- Integer division
+    | otherwise = error "Invalid operation"
 
--- main2 :: IO ()
--- main2 = do
---     print (calculate 10 5 '+')  -- Output: 15
---     print (calculate 10 5 '/')  -- Output: 2
+main2 :: IO ()
+main2 = do
+    print (calculate 10 5 '+')  -- Output: 15
+    print (calculate 10 5 '/')  -- Output: 2
 
 
 -- or
-
+-- Using simple if-else
 calc a b op =if op == '+'
         then a+b
         else if op == '-'
