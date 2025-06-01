28bd0aa18930fb5904370c0b7cc11df88ff5f60c
Akshay KS <143313500+akshayks13@users.noreply.github.com>
2025-06-01 20:02:02 +0530

    Update reverseList.hs

--- reverseList.hs
+++ reverseList.hs
@@ -1,10 +1,9 @@
-
+-- -- Reverse a string
 rev xs = if null xs 
     then xs
     else rev(tail xs) ++ [head xs]
 
 -- Reverse a list
-
 rev1 :: [a] -> [a]
 rev1 [] = []                     
 rev1 (x:xs) = rev1 xs ++ [x]
