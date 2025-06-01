ba1eb60077df59d7fc99cc7a790ef0f1a29550e5
Akshay KS <143313500+akshayks13@users.noreply.github.com>
2025-06-01 19:56:18 +0530

    Update lengthoflist.hs

--- lengthoflist.hs
+++ lengthoflist.hs
@@ -2,7 +2,6 @@ lengthofList xs = if null xs
                   then 0
                   else 1 + lengthofList(tail xs)
                   
-
 -- Using pattern matching
 le [] = 0
 le xs = 1 + le(tail xs)
