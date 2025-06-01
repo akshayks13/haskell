a5849946f0b7816f3afbb49f74b92691be239839
Akshay KS <143313500+akshayks13@users.noreply.github.com>
2025-06-01 19:57:54 +0530

    Update power.hs

--- power.hs
+++ power.hs
@@ -1,4 +1,3 @@
-
 power :: (Num t1, Ord t1, Fractional t2) => t2 -> t1 -> t2
 power x n = if n == 0 
     then 1
@@ -6,7 +5,6 @@ power x n = if n == 0
         if n>0 
             then x * power x (n-1)
         else 1/power x (-n)
-
 
 pow1 x n 
     | n == 0 = 1
